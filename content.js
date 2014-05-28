9a3c7dcb226f5cb1e27aeba75ed6ed356f9d14dd
Chetan Padia <chetan@swiftkey.net>
2014-05-28 13:43:11 +0100

    highlight files when visible

--- content.js
+++ content.js
@@ -1,3 +1,34 @@
-// show diff stats by default
-var toc = document.getElementById('toc');
-toc.className = toc.className + ' open';
+function filterVisible(elems) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var visibleElems = [];
+    $.each(elems, function(i, elem) {
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+        if ((elemBottom >= docViewTop) && (elemTop <= docViewBottom)) {
+            visibleElems.push(elem);
+        }
+    });
+    return visibleElems;
+}
+
+function highlightVisibleFiles() {
+    var visibleSelector = $.map(filterVisible($('.file')), function(elem) {
+        return 'li:has(a[href=#' + $(elem).attr('id') + '])';
+    }).join(',');
+    var toc = $('#toc ol');
+    toc.children('li').removeClass('visible');
+    if (visibleSelector) {
+        toc.children(visibleSelector).addClass('visible');
+    }
+}
+
+$(function() {
+    // show diff stats by default
+    $('#toc').addClass('open');
+
+    // highlight files as the user scrolls
+    $(window).scroll(highlightVisibleFiles);
+    highlightVisibleFiles();
+});
